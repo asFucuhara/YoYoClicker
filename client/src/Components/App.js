030da8e528301156be11f58c2a2ffb52517317ae
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2020-04-20 16:04:29 -0300

    fornt implementation

---
+++ client/src/Components/App.js
@@ -0,0 +1,252 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">Campeonato de Yoyo Online</div>
+      <div className="clicker">
+        <div className="clickerList">
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separator" />
+          <div className="clickerUnit">
+            <img src="https://scontent.fcgh7-1.fna.fbcdn.net/v/t1.0-9/p960x960/39927861_1751477294964842_17347746570698752_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_eui2=AeG7FyPpiHm6aJMesNnH8CnCjtvZDDD_mBKO29kMMP-YEjX08a1_DH_ECakySjw7Rfs&_nc_ohc=2fGRwMweEHQAX-T2U7V&_nc_ht=scontent.fcgh7-1.fna&_nc_tp=6&oh=28a0aad6bd6765aab84590c63a353970&oe=5EC3B154" />
+            <div className="text">
+              <div className="name">
+                perecles da silva gayfasdadsadadasadasdadasdsadadasdsdadsadsaa
+              </div>
+              <div className="score">
+                <div>
+                  <spam className="negative">- </spam>
+                  <spam id="negativeNumber">0</spam>
+                </div>
+                <div>
+                  <spam className="positive">+ </spam>
+                  <spam id="positiveNumber">0</spam>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* auto generate */}
+        </div>
+      </div>
+      <div className="video">
+        {/* add title */}
+        <div className="videoContainer">
+          <iframe
+            src="https://www.youtube.com/embed/3US1fbmwZ40?controls=0"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <div className="control">
+        <div className="counterWrapper">
+          <div className="negativeCounter counter">25</div>
+          <div className="button">space</div>
+        </div>
+        <div className="score">
+          <div>Score</div>
+          <div className="scoreNumber">73</div>
+        </div>
+        <div className="counterWrapper">
+          <div className="positiveCounter counter">108</div>
+          <div className="button">enter</div>
+        </div>
+      </div>
+      <div className="footer">Ideia: Heitor Peres / Design: Hideki / Dev: Shindi</div>
+    </div>
+  );
+}
+
+export default App;
