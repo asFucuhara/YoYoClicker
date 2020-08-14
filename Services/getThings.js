96b04d5f097944a6475a23facc15befd737ffe6d
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2020-08-14 15:46:40 -0300

    minor changes

--- Services/getThings.js
+++ Services/getThings.js
@@ -11,13 +11,12 @@ const scoreModel = mongoose.model('Score');
 const clickHistoryModel = mongoose.model('ClickHistory');
 
 mongoose.connect(
-  process.env.MONGOURL ||
-    'mongodb://a:a12345@ds255347.mlab.com:55347/yoyo-clicker',
+  process.env.MONGOURL || 'mongodb://localhost:27017/YoyoClicker-Dev',
   { useNewUrlParser: true },
-  () => console.log('Db Connected')
+  (error) => (error ? console.error('Db:', error) : console.log('Db Connected'))
 );
 
-(async () => {
+const getScore = async () => {
   const json = await scoreModel
     .find({
       clicks: {
@@ -26,27 +25,26 @@ mongoose.connect(
     })
     .populate('clicker');
   console.log('ok');
-
-  mapa = {
-    WtDjDxdytNU: '1A Felipe',
-    Go7n2Dk_4Y: '1A Patrick Ronckoski',
-    IM2dazr1VCU: '1A Guxtavo amaral',
-    wtNjHnLyZrU: '1A Maicon cunha',
-    VtYLSmqZTsM: '1A Thiago deus smb',
-    K0iEBB0pbys: '1A Marechal',
-    '5nv9WT-ZRIk': '1A Heitor Peres',
-    zpmcS2EUdDg: '1A Guilherme Fragoso',
-    Lbo1Onk7qK: '1A Rafael Yuri',
-    gXmpIVvXLHc: '1A Daniel Borges',
-    '3-Z7uIObFbk': '1A Eduardo Moma',
-    vznSHrBZ7uc: '1A Shindi',
-    Cqlw4YwV740: 'OPEN Guxtavo amaral',
-    NOdeZW1FUCY: 'OPEN Heitor Peres',
-    QyvuryAWMrE: 'OPEN Eduardo Moma',
-    aRMucpcKL0w: 'OPEN Daniel Borges',
-    '-0NFHRZmteo': 'OPEN Guilherme Fragoso',
-    KfnhYg1pXuE: 'OPEN Patrick Ronckoski',
-  };
+  // mapa = {
+  //   WtDjDxdytNU: '1A Felipe',
+  //   Go7n2Dk_4Y: '1A Patrick Ronckoski',
+  //   IM2dazr1VCU: '1A Guxtavo amaral',
+  //   wtNjHnLyZrU: '1A Maicon cunha',
+  //   VtYLSmqZTsM: '1A Thiago deus smb',
+  //   K0iEBB0pbys: '1A Marechal',
+  //   '5nv9WT-ZRIk': '1A Heitor Peres',
+  //   zpmcS2EUdDg: '1A Guilherme Fragoso',
+  //   Lbo1Onk7qK: '1A Rafael Yuri',
+  //   gXmpIVvXLHc: '1A Daniel Borges',
+  //   '3-Z7uIObFbk': '1A Eduardo Moma',
+  //   vznSHrBZ7uc: '1A Shindi',
+  //   Cqlw4YwV740: 'OPEN Guxtavo amaral',
+  //   NOdeZW1FUCY: 'OPEN Heitor Peres',
+  //   QyvuryAWMrE: 'OPEN Eduardo Moma',
+  //   aRMucpcKL0w: 'OPEN Daniel Borges',
+  //   '-0NFHRZmteo': 'OPEN Guilherme Fragoso',
+  //   KfnhYg1pXuE: 'OPEN Patrick Ronckoski',
+  // };
 
   const processed = json.map((object) => {
     console.log(object);
@@ -73,4 +71,4 @@ mongoose.connect(
     }
   });
   console.log(JSON.stringify(processed));
-})();
+};
